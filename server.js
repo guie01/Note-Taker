cfb77e306e786a74a9891ca37cb2c993ea987ef0
Guillermo Garcia <ggq0107@hotmail.com>
2021-03-06 20:12:52 -0500

    correctly listening to port, app is able to get all pages correctly

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
 })
