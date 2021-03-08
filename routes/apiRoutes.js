3cee693c1deca8e44af7cf6b9daf6deb8241cdd8
Guillermo Garcia <ggq0107@hotmail.com>
2021-03-08 16:45:23 -0500

    post is working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,15 @@
 const database = require('../db/db.json');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(database));
-    app.post('api/notes', (req,res) => {
+    app.post('/api/notes', (req,res) => {
+        let objID = database[database.length - 1].id
+        req.body.id = objID + 1;
         database.push(req.body);
         res.json(true);
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(database), (err) =>
+        err ? console.log(err) : console.log('Success!'))
     });
 }
